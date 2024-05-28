d097b2175617bc27d233857295acc2dde357bdf5
adinyaev <v.adinyaev5@gmail.com>
2024-05-28 13:49:16 +0300

    writing encrypted

--- chua_example.cpp
+++ chua_example.cpp
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #include "receiver/include/main.h"
 #include "transmitter/include/main.h"
 #include "yaml-cpp/yaml.h"
+
+void write_signal(std::ofstream &f, std::vector<double> signal, std::string name)
+{
+    f << name << ": [";
+    for (const auto &s : signal)
+    {
+        f << s;
+        if (&s != &signal.back())
+            f << ",";
+    }
+    f << "]" << std::endl;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -18,10 +32,15 @@ int main(int argc, char const *argv[])
     if (input["signal"])
         data = input["signal"].as<std::vector<int>>();
 
-    std::cout << data[1] << std::endl;
-    // auto signal = transmit(data);
+    auto signal = transmit(data);
+    auto estimation = observe(signal[0], signal[1], signal[2]);
 
-    // auto estimation = observe(signal[0], signal[1], signal[2]);
+    std::ofstream out_encrypted;
+    out_encrypted.open("../encrypted_signal.yaml");
+    write_signal(out_encrypted, signal[0], "x1");
+    write_signal(out_encrypted, signal[1], "x2");
+    write_signal(out_encrypted, signal[2], "x3");
+    out_encrypted.close();
 
     return 0;
 }
