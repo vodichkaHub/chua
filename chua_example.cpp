6aa3a5ef1f79313f047eedbb4941a914c168c886
adinyaev <v.adinyaev5@gmail.com>
2024-05-28 13:31:25 +0300

    reading yaml

--- chua_example.cpp
+++ chua_example.cpp
@@ -7,10 +7,21 @@
 
 int main(int argc, char const *argv[])
 {
-    std::vector<int> data = {0, 1, 0, 0, 1, 1};
-    auto signal = transmit(data);
+    if (argc < 2)
+    {
+        std::cerr << "No input file path!" << std::endl;
+        return 1;
+    }
 
-    auto estimation = observe(signal[0], signal[1], signal[2]);
+    YAML::Node input = YAML::LoadFile(argv[1]);
+    std::vector<int> data;
+    if (input["signal"])
+        data = input["signal"].as<std::vector<int>>();
+
+    std::cout << data[1] << std::endl;
+    // auto signal = transmit(data);
+
+    // auto estimation = observe(signal[0], signal[1], signal[2]);
 
     return 0;
 }
