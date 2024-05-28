f2bbd5d2b63f6570a188fb410141a88739e685e9
adinyaev <v.adinyaev5@gmail.com>
2024-05-28 12:23:31 +0300

    example done

--- chua_example.cpp
+++ chua_example.cpp
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <iostream>
 
-// #include "receiver/include/main.h"
+#include "receiver/include/main.h"
 #include "transmitter/include/main.h"
 
 int main(int argc, char const *argv[])
 {
     Transmitter transmitter;
-    std::vector<uint8_t> data = {0, 1, 0, 0, 1, 1};
-    transmitter.transmit(data);
+    std::vector<int> data = {0, 1, 0, 0, 1, 1};
+    auto signal = transmitter.transmit(data);
+
+    Receiver receiver;
+    auto estimation = receiver.observe(signal[0], signal[1], signal[2]);
+    for (int b : estimation)
+        std::cout << b <<  " ";
+
     return 0;
 }
