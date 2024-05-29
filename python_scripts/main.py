db8ed39ba161ed51f7278a9bb259fdff01ef3f84
adinyaev <v.adinyaev5@gmail.com>
2024-05-29 11:06:21 +0300

    plots

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -13,14 +13,7 @@ class ExampleApp(QtWidgets.QMainWindow, chua_ui.Ui_MainWindow):
         self.setupUi(self)
         self.runButton.clicked.connect(self.startFunc)
 
-    def startFunc(self):
-        # Получаем текст
-        textin = self.userTextInput.toPlainText()
-        # print(textin)
-        #Делаем бинарным каждый символ и по букву объединяет в списки
-        bytelist = [list(format(ord(x), 'b')) for x in textin]
-        # print(bytelist)
-
+    def modelRun(self, bytesin: list) -> list:
         # simulink
         bytelist_out = []
         chanel_data_plot = {'x1': [], 'x2': [], 'x3': []}
@@ -29,12 +22,38 @@ class ExampleApp(QtWidgets.QMainWindow, chua_ui.Ui_MainWindow):
         chanel_data_file = os.path.join(cur_dir, '..', 'run', 'encrypted_signal.yaml')
         restored_file = os.path.join(cur_dir, '..', 'run', 'restored_signal.yaml')
         model_exe  =  os.path.join(cur_dir, '..', 'build', 'example')
-        for byte_letter in bytelist:
+        for byte_letter in bytesin:
             simulink.makeInputFile(input_file, {'signal': byte_letter})
             simulink.runModel(model_exe, input_file, chanel_data_file, restored_file)
             bytelist_out.append(simulink.getRestoredSignal(restored_file))
             # chanel_data = simulink.getChanelData(chanel_data_file)
+            # chanel_data_plot['x1'].extend(chanel_data['x1'])
+            # chanel_data_plot['x2'].extend(chanel_data['x2'])
+            # chanel_data_plot['x3'].extend(chanel_data['x3'])
         # simulink
+
+        # names = ['x1', 'x2', 'x3']
+        # samples = range(0,len(chanel_data_plot['x1']))
+        # fig = plt.figure()
+        # axes = fig.subplots(nrows=3, ncols=1, sharey=True)
+        # for i in range(0,3):
+        #     axes[i].plot(samples, chanel_data_plot[names[i]])
+        #     axes[i].grid(True)
+        # plt.draw()
+        # plt.pause(.001)
+
+        return bytelist_out
+
+    def startFunc(self):
+        # Получаем текст
+        textin = self.userTextInput.toPlainText()
+        # print(textin)
+        #Делаем бинарным каждый символ и по букву объединяет в списки
+        bytelist = [list(format(ord(x), 'b')) for x in textin]
+        # print(bytelist)
+
+        # simulink model
+        bytelist_out = self.modelRun(bytelist)
 
         # Отдаём текст, превращая из bytelist в нормальное слово
         textout = ''.join(chr(int(''.join(byte), 2)) for byte in bytelist_out)
--- python_scripts/simulink.py
+++ python_scripts/simulink.py
@@ -14,11 +14,12 @@ def getRestoredSignal(restored_filename: str) -> list:
         data = yaml.safe_load(stream)['restored']
         return list(map(str, data))
 
+# HEAVY FUNCTION!!! time consuming
 def getChanelData(chanel_signal_filename) -> dict:
     chanel_data = {'x1': [], 'x2': [], 'x3': []}
     with open(chanel_signal_filename, 'r') as stream:
         yaml_data = yaml.safe_load(stream)
-        chanel_data['x1'] = yaml_data['x1']
-        chanel_data['x2'] = yaml_data['x2']
-        chanel_data['x3'] = yaml_data['x3']
+        chanel_data['x1'] = [yaml_data['x1'][i] for i in range(len(yaml_data['x1'])) if i % 1000 == 0]
+        chanel_data['x2'] = [yaml_data['x2'][i] for i in range(len(yaml_data['x1'])) if i % 1000 == 0]
+        chanel_data['x3'] = [yaml_data['x3'][i] for i in range(len(yaml_data['x1'])) if i % 1000 == 0]
     return chanel_data
